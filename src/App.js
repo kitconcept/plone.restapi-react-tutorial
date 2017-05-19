926e95dd58a118d8bf1cd1c9280a2396ff31e17e
Timo Stollenwerk <stollenwerk@kitconcept.com>
2017-05-19 10:23:44 +0200

    Add AppContainer component that pulls data from plone.rstapi backend.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_URL = 'http://localhost:8080/Plone';
+const API_HEADERS = {
+  'Accept': 'application/json',
+  'Authorization': 'Basic Zm9vYmFyOmZvb2Jhcgo='
+};
+
+
+class AppContainer extends Component {
+  constructor(){
+    super();
+    this.state={
+      page: {}
+    };
+  }
+
+  componentDidMount(){
+    fetch(API_URL + '/front-page', {headers: API_HEADERS})
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({page: responseData});
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
+  render(){
+    return (
+      <App page={this.state.page} />
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{this.props.page.title}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.props.page.description &&
+          <h3>{this.props.page.description}</h3>
+        }
+        {
+          this.props.page.text &&
+          <p dangerouslySetInnerHTML={{ __html: this.props.page.text.data }} />
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppContainer from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <AppContainer />,
   document.getElementById('root')
 );
